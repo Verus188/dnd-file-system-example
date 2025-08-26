f35ea33b1000c954cc96a8c3d942dcaf8e940b28
Nikita <king_king_03@bk.ru>
2025-08-26 12:04:37 +0300

    изменение стиля кнопки открытия папки

--- src/renderer/src/components/button.tsx
+++ src/renderer/src/components/button.tsx
@@ -7,7 +7,7 @@ export const Button: FC<ButtonProps> = ({ className, children, ...props }) => {
   return (
     <button
       className={twMerge(
-        'px-2 h-fit rounded-lg text-[var(--color-text-orange)] hover:bg-[var(--color-background-dark-blue-hovered)] flex items-center justify-center border-4 border-[var(--color-border-dark-blue)] bg-[var(--color-background-dark-blue)] active:translate-y-0.5',
+        'px-2 h-fit rounded-lg text-[var(--color-background-dark-blue)] hover:opacity-90 flex items-center justify-center border-2 border-[var(--color-border-dark-blue)] bg-[var(--color-text-orange)] active:translate-y-0.5',
         className
       )}
       {...props}
